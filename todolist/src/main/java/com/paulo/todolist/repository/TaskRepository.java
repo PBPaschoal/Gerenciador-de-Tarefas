8988338885a412494d656d858ef6e4a1de37ce01
Paulo Paschoal <programming.paschoal@gmail.com>
2024-12-26 21:36:20 -0300

    API Finalizada e testada com Postman: GET, POST, PUT E DEL OK

--- todolist/src/main/java/com/paulo/todolist/repository/TaskRepository.java
+++ todolist/src/main/java/com/paulo/todolist/repository/TaskRepository.java
@@ -2,7 +2,9 @@ package com.paulo.todolist.repository;
 
 import com.paulo.todolist.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
 }
+
